9be12fe827a828ed616a698ee54f52dfea4d7acb
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2014-12-01 16:53:11 -0300

    Concertado o tratamento de Threads ao aplicativo ser minimizado.

--- OnibusSocial/src/com/joaoemedeiros/onibussocial/MapFragment.java
+++ OnibusSocial/src/com/joaoemedeiros/onibussocial/MapFragment.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,201 +31,240 @@ import com.joaoemedeiros.onibussocial.mysql.ConnectorImpl;
 import com.joaoemedeiros.onibussocial.mysql.OnibusDAO;
 
 public class MapFragment extends Fragment {
-    /**
-     * The fragment argument representing the section number for this
-     * fragment.
-     */
-    private static final String ARG_SECTION_NUMBER = "section_number";
-    private GoogleMap googleMap;
-    private Spinner spinner;
-    private Connector connect;
-    private OnibusDAO onibusDAO;
-    private List<Onibus> onibus;
+	/**
+	 * The fragment argument representing the section number for this fragment.
+	 */
+	private static final String ARG_SECTION_NUMBER = "section_number";
+	private GoogleMap googleMap;
+	private Spinner spinner;
+	private Connector connect;
+	private OnibusDAO onibusDAO;
+	private List<Onibus> onibus;
 	private int onibus_selecionado;
-    Thread threadMapa;
+	Thread threadMapa;
 
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
-    public static MapFragment newInstance(int sectionNumber) {
-        MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-        fragment.setArguments(args);
-        return fragment;
-    }
-    
-    @Override
-    public void onDestroy() {
-    	threadMapa.interrupt();
-    	super.onDestroy();
-    }
+	/**
+	 * Returns a new instance of this fragment for the given section number.
+	 */
+	public static MapFragment newInstance(int sectionNumber) {
+		MapFragment fragment = new MapFragment();
+		Bundle args = new Bundle();
+		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+		fragment.setArguments(args);
+		return fragment;
+	}
 
-    public MapFragment() {
-    }
+	@Override
+	public void onPause() {
+		threadMapa.interrupt();
+		super.onPause();
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-        spinner = (Spinner) rootView.findViewById(R.id.spinner1);
-        
-        connect = new ConnectorImpl();
-        onibusDAO = OnibusDAO.getDAO();
+	@Override
+	public void onResume() {
+		if (threadMapa.getState() == Thread.State.NEW) { 
+			Log.d("Thread", "não teve que ser recriada");
+			threadMapa.start();
+		} else {
+			Log.d("Thread", "teve que ser recriada");
+			iniciarThread();
+			threadMapa.start();
+		}
+		
+		super.onResume();
+	}
 
-        try {
+	public MapFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_map, container,
+				false);
+		spinner = (Spinner) rootView.findViewById(R.id.spinner1);
+
+		connect = new ConnectorImpl();
+		onibusDAO = OnibusDAO.getDAO();
+
+		try {
 			onibusDAO.populate(getActivity());
 		} catch (UnconnectedException e1) {
 			e1.printStackTrace();
-			Toast.makeText(getActivity(), "OnibusSocial: Verifique sua conexão com Internet", Toast.LENGTH_LONG).show();
+			Toast.makeText(getActivity(),
+					"OnibusSocial: Verifique sua conexão com Internet",
+					Toast.LENGTH_LONG).show();
 			MainActivity.isConnected();
 		}
-        
-        /**
-         * Thread para popular marcadores 
-         */
-        threadMapa = new Thread(new Runnable() {
+
+		iniciarThread();
+		carregarSpinner();
+		initilizeMap();
+		googleMap.setMyLocationEnabled(true);
+		googleMap.getUiSettings().setZoomControlsEnabled(false);
+		carregarMarcadores();
+		return rootView;
+	}
+	
+	public void iniciarThread() {
+		/**
+		 * Thread para popular marcadores
+		 */
+		threadMapa = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				while(!Thread.currentThread().isInterrupted()) {
-					
-					//Para matar a thread quando o usuário fechar o app
-					if(getActivity() == null)
-						break;
-					getActivity().runOnUiThread(new Runnable() {
-						
-						@Override
-						public void run() {
-							limparMapa();
-						}
-					});
-					List<Localizacao> list = null;
+				boolean stop = false;
+				while (!stop) {
 					try {
-						list = connect.getHTTPLocalizacao(onibus_selecionado);
-					} catch (UnconnectedException e1) {
-						e1.printStackTrace();
+						// Para matar a thread quando o usuário fechar o app
+						if (getActivity() == null)
+							break;
 						getActivity().runOnUiThread(new Runnable() {
-							
+	
 							@Override
 							public void run() {
-								Toast.makeText(getActivity(), "Sem conexão, não será possível ver ônibus.",Toast.LENGTH_SHORT).show();
+								limparMapa();
 							}
 						});
-					}
-					if(list != null)
-						for(Localizacao loc : list) {
-							String[] parts = loc.getLocalizacao().split(":");
-							final double latitude = Double.parseDouble(parts[0]);
-							final double longitude = Double.parseDouble(parts[1]);
-							final Onibus onibus = onibusDAO.getOnibus(loc.getId_onibus(), getActivity());
-							if(getActivity() == null)
-								break;
+						List<Localizacao> list = null;
+						try {
+							list = connect
+									.getHTTPLocalizacao(onibus_selecionado);
+						} catch (UnconnectedException e1) {
+							e1.printStackTrace();
 							getActivity().runOnUiThread(new Runnable() {
-								
+	
 								@Override
 								public void run() {
-									addMarcador(latitude, longitude, onibus.getLinha(), onibus.getNomeEmpresa());
+									Toast.makeText(
+											getActivity(),
+											"Sem conexão, não será possível ver ônibus.",
+											Toast.LENGTH_SHORT).show();
 								}
 							});
 						}
-					try {
+						if (list != null)
+							for (Localizacao loc : list) {
+								String[] parts = loc.getLocalizacao()
+										.split(":");
+								final double latitude = Double
+										.parseDouble(parts[0]);
+								final double longitude = Double
+										.parseDouble(parts[1]);
+								final Onibus onibus = onibusDAO.getOnibus(
+										loc.getId_onibus(), getActivity());
+								if (getActivity() == null)
+									break;
+								getActivity().runOnUiThread(new Runnable() {
+	
+									@Override
+									public void run() {
+										addMarcador(latitude, longitude,
+												onibus.getLinha(),
+												onibus.getNomeEmpresa());
+									}
+								});
+							}
 						Thread.sleep(5000);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
+						stop = true;
+						Log.d("Thread", "Thread foi stopped");
 					}
 				}
 			}
 		});
+	}
 
-        carregarSpinner();
-        initilizeMap();
-        googleMap.setMyLocationEnabled(true);
-        googleMap.getUiSettings().setZoomControlsEnabled(false);
-        carregarMarcadores();
-        return rootView;
-    }
-    
-    private void carregarSpinner() {
-    	
-    	List<String> list = new ArrayList<String>();
-    	onibus = onibusDAO.getAllOnibus(getActivity());
-    	
-    	list.add("Selecione um Ônibus");
-    	for(Onibus bus : onibus) {
-    		list.add(bus.getLinha() + "/" + bus.getNomeEmpresa());
-    	}
-    	
-    	ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, list);
-    	
-    	dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    	spinner.setAdapter(dataAdapter);
-    	 
-    	spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+	private void carregarSpinner() {
+
+		List<String> list = new ArrayList<String>();
+		onibus = onibusDAO.getAllOnibus(getActivity());
+
+		list.add("Selecione um Ônibus");
+		for (Onibus bus : onibus) {
+			list.add(bus.getLinha() + "/" + bus.getNomeEmpresa());
+		}
+
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(
+				getActivity(), android.R.layout.simple_spinner_item, list);
+
+		dataAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(dataAdapter);
+
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				if(arg2 > 0) {
+				if (arg2 > 0) {
 					Onibus bus = onibus.get(arg2 - 1);
 					onibus_selecionado = bus.getId();
 				} else {
 					onibus_selecionado = 0;
 				}
 			}
-	
+
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {
-					
+
 			}
-    		 
-    	});
+
+		});
 	}
 
 	private void carregarMarcadores() {
-		if(connect == null)
+		if (connect == null)
 			connect = new ConnectorImpl();
-		threadMapa.start();
 	}
-	
+
 	public void limparMapa() {
 		googleMap.clear();
 	}
-	
-	public void addMarcador(double latitude, double longitude, String linha, String empresa) {
+
+	public void addMarcador(double latitude, double longitude, String linha,
+			String empresa) {
 		// create marker
-		MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude))
-												  .title(linha)
-												  .snippet(empresa)
-												  .icon(BitmapDescriptorFactory.fromResource(R.drawable.onibus_map));
+		MarkerOptions marker = new MarkerOptions()
+				.position(new LatLng(latitude, longitude))
+				.title(linha)
+				.snippet(empresa)
+				.icon(BitmapDescriptorFactory
+						.fromResource(R.drawable.onibus_map));
 		googleMap.addMarker(marker);
 	}
 
 	private void initilizeMap() {
-    	if (googleMap == null) {
-            googleMap = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
- 
-            // check if map is created successfully or not
-            if (googleMap == null) {
-                Toast.makeText(getActivity().getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                        .show();
-            }
-            
-            googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-                @Override
-                public void onMyLocationChange(Location location) {
+		if (googleMap == null) {
+			googleMap = ((SupportMapFragment) getActivity()
+					.getSupportFragmentManager().findFragmentById(R.id.map))
+					.getMap();
 
-                        CameraUpdate center=CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
-                        CameraUpdate zoom=CameraUpdateFactory.zoomTo(13);
-                        googleMap.moveCamera(center);
-                        googleMap.animateCamera(zoom);
+			// check if map is created successfully or not
+			if (googleMap == null) {
+				Toast.makeText(getActivity().getApplicationContext(),
+						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
+						.show();
+			}
 
-                }
+			googleMap
+					.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+						@Override
+						public void onMyLocationChange(Location location) {
 
-            });
-        }
+							CameraUpdate center = CameraUpdateFactory
+									.newLatLng(new LatLng(location
+											.getLatitude(), location
+											.getLongitude()));
+							CameraUpdate zoom = CameraUpdateFactory.zoomTo(13);
+							googleMap.moveCamera(center);
+							googleMap.animateCamera(zoom);
+
+						}
+
+					});
+		}
 	}
 
 }
